0446439f66d2e591784b7e6b9a76cd54fa43d5a4
Vimelia <58261422+Vimelia@users.noreply.github.com>
2022-02-22 03:26:12 +0300

    Project added

---
+++ code/c_01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vdegirme <vdegirme@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/19 11:17:55 by vdegirme          #+#    #+#             */
+/*   Updated: 2022/02/19 13:12:09 by vdegirme         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	temp;
+
+	while (size >= 0)
+	{
+		i = 0;
+		while (i < size -1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				temp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = temp;
+			}
+			i++;
+		}
+		size--;
+	}
+}
